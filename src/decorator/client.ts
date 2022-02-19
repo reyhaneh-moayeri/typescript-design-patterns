8a1c888c056c34592d62add45f5fc710d0a49587
reyhaneh <reyhanehrm333@gmail.com>
2022-02-19 09:34:25 +0330

    Decorator pattern

---
+++ src/decorator/client.ts
@@ -0,0 +1,18 @@
+/*
+
+the decorator pattern is a structural pattern that allows you to attach 
+additional responsibility to an object at runtime
+
+it is used in both object oriented programming and Functional paradigms
+
+the decorator pattern adds extensibility without modifying the original object
+
+
+*/
+
+import Value from "./value";
+import Add from "./add";
+const A = Value(1);
+const B = Value(4);
+
+console.log(Add(A, B).value); // 5
