14926b8783b66ca3a752bea30ddf92ac3104ab7b
reyhaneh <reyhanehrm333@gmail.com>
2022-02-20 15:52:22 +0330

    update decorator

--- src/decorator/client.ts
+++ src/decorator/client.ts
@@ -18,3 +18,7 @@ const B = Value(4);
 
 console.log(Add(A, B).value); // 5
 console.log(Sub(B, A).value); // 3
+console.log(Sub(Add(B, B), B).value); // 4
+
+// use decorator when you want to add responsibilities to objects dynamically without effect the inner object
+// you need an option to later remove the decorator from an objectin case you no longer need it
