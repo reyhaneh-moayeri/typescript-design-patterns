f57412de6181966e97d60ca3c6347f9ae1eebe76
reyhaneh <reyhanehrm333@gmail.com>
2022-02-21 02:32:49 +0330

    bridge concept

---
+++ src/Bridge/bridge-concept.ts
@@ -0,0 +1,58 @@
+// Bridge Pattern Concept Sample Code
+
+interface IAbstraction {
+  method(value: string[]): void;
+}
+
+class RefinedAbstractionA implements IAbstraction {
+  private implementer: IImplementer;
+
+  constructor(implementer: IImplementer) {
+    this.implementer = implementer;
+  }
+
+  method(value: string[]) {
+    this.implementer.method(value);
+  }
+}
+
+class RefinedAbstractionB implements IAbstraction {
+  private implementer: IImplementer;
+
+  constructor(implementer: IImplementer) {
+    this.implementer = implementer;
+  }
+
+  method(value: string[]) {
+    this.implementer.method(value);
+  }
+}
+
+interface IImplementer {
+  method(value: string[]): void;
+}
+
+class ConcreteImplementerA implements IImplementer {
+  method(value: string[]) {
+    console.log(value);
+  }
+}
+
+class ConcreteImplementerB implements IImplementer {
+  method(value: string[]) {
+    value.forEach((v) => console.log(v));
+  }
+}
+
+// The Client
+const VALUES = ["a", "b", "c"];
+
+const REFINED_ABSTRACTION_A = new RefinedAbstractionA(
+  new ConcreteImplementerA()
+);
+REFINED_ABSTRACTION_A.method(VALUES);
+
+const REFINED_ABSTRACTION_B = new RefinedAbstractionB(
+  new ConcreteImplementerB()
+);
+REFINED_ABSTRACTION_B.method(VALUES);
